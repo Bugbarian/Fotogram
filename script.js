a1082f9b139678e78665e36583242940240220d0
Matze <you@example.comm.tomaschek.jr@gmail.com>
2026-06-14 17:48:30 +0200

    added js list and render function

---
+++ script.js
@@ -0,0 +1,30 @@
+let pictureList = [
+    "pic_1.png",
+    "pic_2.png",
+    "pic_3.png",
+    "pic_4.png",
+    "pic_5.png",
+    "pic_6.png",
+    "pic_7.png",
+    "pic_8.png",
+    "pic_9.png",
+    "pic_10.png",
+    "pic_11.png",
+    "pic_12.png" 
+];
+
+function getPictures(i){
+    return `<div class="single_picture">
+                <img src="./assets/pictures/${pictureList[i]}" alt="Bild">
+            </div>`;            
+}
+
+function renderPictures() {
+    let generatePictures = document.getElementById("picture_box");
+    for (let i = 0; i < pictureList.length; i++){
+        generatePictures.innerHTML += getPictures(i);
+    }
+}
+
+
+
